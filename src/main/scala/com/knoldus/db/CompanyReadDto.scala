e3f6419fa2ac7e31c10eb6fe4c1bc213a5887e58
Vineet Chauhan <86958280+veechauhan@users.noreply.github.com>
2021-09-14 23:20:38 +0530

    Update CompanyReadDto.scala

--- src/main/scala/com/knoldus/db/CompanyReadDto.scala
+++ src/main/scala/com/knoldus/db/CompanyReadDto.scala
@@ -7,8 +7,8 @@ import scala.collection.immutable.HashMap
 class CompanyReadDto {
 
   val knoldusCompany: Company = Company("Knoldus", "knoldus@gmail.com", "Noida")
-  val philipsCompany: Company = Company("Philips", "philips123@gmail.com", "Noida")
-  val companies: HashMap[String, Company] = HashMap("Knoldus" -> knoldusCompany, "Philips" -> philipsCompany)
+  val cloudCompany: Company = Company("Cloud", "cloud123@gmail.com", "Noida")
+  val companies: HashMap[String, Company] = HashMap("Knoldus" -> knoldusCompany, "cloud" -> cloudCompany)
 
   def getCompanyByName(name: String): Option[Company] = companies.get(name)
 
